0257faf51e9dd65f47f71e5c4ed313da82490e65
Vinnie - Bart Stoebel <Bart.Stoebel@gmail.com>
2017-02-23 14:07:01 +0100

    GastenboekEntry geschreven

---
+++ src/be/vdab/exception/NewEntryException.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.vdab.exception;
+
+/**
+ *
+ * @author Vinnie
+ */
+public class NewEntryException extends Exception{
+    private String message;
+    
+    public NewEntryException(String error, String message){
+        super(error);
+        setMessage(message);
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    
+    
+}
