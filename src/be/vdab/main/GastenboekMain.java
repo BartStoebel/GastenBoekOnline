295e6a81a684f1f6c69466a457a317c564abc87e
Vinnie - Bart Stoebel <Bart.Stoebel@gmail.com>
2017-02-24 13:32:34 +0100

    Main aanpassen

--- src/be/vdab/main/GastenboekMain.java
+++ src/be/vdab/main/GastenboekMain.java
@@ -21,32 +21,42 @@ public class GastenboekMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        try{
-            GastenboekManager gbm = new GastenboekManager();
-            String naam = "";
-            String boodschap;
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Typ je naam:");
-            naam = scanner.nextLine();
-
-            while (naam!=null && 
-                    !naam.equals("")){
-                System.out.println("Typ je boodschap:");
-                boodschap = scanner.nextLine();
-                gbm.voegEntryToe(naam, boodschap);
-                System.out.println("Typ je naam:");
-                naam = scanner.nextLine();
-            }
-            
-            
-            for (GastenboekEntry ge : gbm.geefAlleEntriesRecenteEerst()){
-                System.out.println(ge.toString());
-            }
-        }catch(IOException e){
-            System.err.println("be.vdab.main.GastenboekMain.main()" +  e.getMessage());
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Geef je keuze in: \n   * 1 voor lezen van gastenboek"
+                + "\n   * 2 voor nieuwe boodschap ingeven \n   * 3 voor einde");
+        String input = scanner.nextLine();
+        while(input.equals("1") && input.equals("2") && input.equals("3")){
+            System.out.println("Geef een correcte keuze in: \n   * 1 voor lezen van gastenboek"
+                + "\n   * 2 voor nieuwe boodschap ingeven \n   * 3 voor einde");
+            input = scanner.nextLine();
         }
-        
-        
+        while (input == "1" || input == "2"){
+            try{
+                GastenboekManager gbm = new GastenboekManager();
+                if (input == "1"){  
+                    for (GastenboekEntry ge : gbm.geefAlleEntriesRecenteEerst()){
+                        System.out.println(ge.toString());
+                    }
+                } else if(input == "2"){
+                    String naam = "";
+                    String boodschap;
+                    System.out.println("Typ je naam:");
+                    naam = scanner.nextLine();
+                    while (naam!=null && !naam.equals("")){
+                        System.out.println("Typ je boodschap:");
+                        boodschap = scanner.nextLine();
+                        gbm.voegEntryToe(naam, boodschap);
+                        System.out.println("Typ je naam:");
+                        naam = scanner.nextLine();
+                    }
+                }
+
+            } catch (IOException e){
+                System.err.println("be.vdab.main.GastenboekMain.main()" +  e.getMessage());
+            }
+            System.out.println("Geef je keuze in: \n   * 1 voor lezen van gastenboek"
+                    + "\n   * 2 voor nieuwe boodschap ingeven \n   * 3 voor einde");
+            input = scanner.nextLine();
+        }
     }
-    
 }
